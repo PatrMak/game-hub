bf374fda7a44f13170338bc38535520c2b71aad4
Patryk Makulski <patryk.makulski@interia.pl>
2023-10-28 18:45:57 +0200

    Added: Fetch game details.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,3 @@
-
 import APIClient, { FetchResponse } from "../services/apiClient";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Platform } from "./usePlatforms";
@@ -9,16 +8,18 @@ const apiClient = new APIClient<Game>("/games");
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
 }
 
-const useGames = () =>{
-  const gameQuery = useGameQueryStore(s=>s.gameQuery);
+const useGames = () => {
+  const gameQuery = useGameQueryStore((s) => s.gameQuery);
 
- return useInfiniteQuery<FetchResponse<Game>, Error>({
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
@@ -35,7 +36,6 @@ const useGames = () =>{
     },
     staleTime: ms("24h"),
   });
-}
-
+};
 
 export default useGames;
