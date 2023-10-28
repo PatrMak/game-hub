bb35d5497033a2e82c663eb25364cd0a645c4f8b
Patryk Makulski <patryk.makulski@interia.pl>
2023-10-28 21:05:26 +0200

    Fixed: search on details page.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,16 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Button, HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearachInput from "./SearachInput";
-
-
+import { Link, Navigate } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
-      <Image src={logo} boxSize="60px" />
-      <SearachInput  />
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover" />
+      </Link>
+      <SearachInput />
       <ColorModeSwitch />
     </HStack>
   );
